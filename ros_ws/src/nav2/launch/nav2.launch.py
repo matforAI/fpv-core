cc3ef0203a88ef118d0c12410431ff2cc7d9df1b
matforAI <matfor.bpla@gmail.com>
2026-02-01 19:47:29 +0000

    Add Nav2 configuration for GPS-denied navigation

---
+++ ros_ws/src/nav2/launch/nav2.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    nav2_dir = get_package_share_directory('nav2_bringup')
+
+    return LaunchDescription([
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(nav2_dir, 'launch', 'bringup_launch.py')
+            ),
+            launch_arguments={
+                'use_sim_time': 'false',
+                'params_file': 'ros_ws/src/nav2/config/nav2.yaml'
+            }.items()
+        )
+    ])
